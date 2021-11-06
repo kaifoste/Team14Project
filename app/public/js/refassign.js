92a2bd1e0dd352f972a2cde7bbdc047c83fd9e31
kaifoste <fosterk202@gmail.com>
2021-11-05 20:20:34 -0400

    assignment things

---
+++ app/public/js/refassign.js
@@ -0,0 +1,114 @@
+const SomeApp = {
+    data() {
+      return {
+        assignments: [],
+        assignmentForm:{},
+        selectedAssignment: null,
+  
+      }
+    },
+    computed: {},
+    methods: {
+        fetchAssignmentsData() {
+            fetch('/api/assignment/')
+            .then( response => response.json() )
+            .then( (responseJson) => {
+                console.log(responseJson);
+                this.assignments = responseJson;
+            })
+            .catch( (err) => {
+                console.error(err);
+            })
+        },
+        selectAssignments(s) {
+          if (s == this.selectedAssignment) {
+              return;
+          }
+          this.selectedAssignment = s;
+          this.assignments = [];
+          this.fetchAssignmentsData(this.selectedAssignments);
+      },
+      postAssignment(evt) {
+        if (this.selectedAssignment === null) {
+            this.postNewAssignment(evt);
+        } else {
+            this.postEditAssignment(evt);
+        }
+      },
+        postNewAssignment(evt) {        
+          console.log("Posting!", this.assignmentForm);
+  
+          fetch('api/assignment/create.php', {
+              method:'POST',
+              body: JSON.stringify(this.assignmentForm),
+              headers: {
+                "Content-Type": "application/json; charset=utf-8"
+              }
+            })
+            .then( response => response.json() )
+            .then( json => {
+              console.log("Returned from post:", json);
+              // TODO: test a result was returned!
+              this.assignments = json;
+              
+              // reset the form
+              this.assignmentForm = {};
+            });
+        },
+        postEditAssignment(evt) {        
+        //   this.matchForm.field = this.selectedMatch.field;        
+          console.log("Updating!", this.assignmentForm);
+  
+          fetch('api/assignment/update.php', {
+              method:'POST',
+              body: JSON.stringify(this.assignmentForm),
+              headers: {
+                "Content-Type": "application/json; charset=utf-8"
+              }
+            })
+            .then( response => response.json() )
+            .then( json => {
+              console.log("Returned from post:", json);
+              // TODO: test a result was returned!
+              this.assignments = json;
+              
+              this.resetAssignmentForm();
+            });
+        },
+        postDeleteAssignment(o) {
+          if (!confirm("Are you sure you want to delete the assignment?")) {
+              return;
+          }
+          
+          fetch('api/assignment/delete.php', {
+              method:'POST',
+              body: JSON.stringify(o),
+              headers: {
+                "Content-Type": "application/json; charset=utf-8"
+              }
+            })
+            .then( response => response.json() )
+            .then( json => {
+              console.log("Returned from post:", json);
+              // TODO: test a result was returned!
+              this.assignments = json;
+              
+              this.resetAssignmentsForm();
+            });
+        },
+        selectAssignmentForEdit(o) {
+          this.selectedAssignment = o;
+          this.assignmentForm = Object.assign({}, this.selectedAssignment);
+      },
+        resetAssignmentForm() {
+          this.selectedAssignment = null;
+          this.assignmentForm = {};
+        },
+        },
+    created() {
+        this.fetchAssignmentsData();
+    }
+  
+  }
+  
+  Vue.createApp(SomeApp).mount('#assignmentApp');
