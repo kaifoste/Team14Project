14de45599f8fe09d2802832c8cfff08a3af7b25e
MykahN <89401136+MykahN@users.noreply.github.com>
2021-11-06 16:19:08 -0400

    Create refmatchhistory report php

---
+++ app/public/api/match/refmatchhistory.php
@@ -0,0 +1,21 @@
+<?php
+require 'class/DbConnection.php';
+
+// Step 1: Get a datase connection from our helper class
+$db = DbConnection::getConnection();
+
+// Step 2: Create & run the query
+$sql = 'SELECT * FROM matches WHERE EXISTS (SELECT * FROM assignments WHERE status="Unassigned"';
+$vars = [];
+
+$stmt = $db->prepare($sql);
+$stmt->execute($vars);
+
+$matches = $stmt->fetchAll();
+
+// Step 3: Convert to JSON
+$json = json_encode($matches, JSON_PRETTY_PRINT);
+
+// Step 4: Output
+header('Content-Type: application/json');
+echo $json;
