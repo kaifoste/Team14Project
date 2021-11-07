feeea1eb9f7f50e57d4d84730d226b508e7359ca
kaifoste <fosterk202@gmail.com>
2021-11-06 21:05:12 -0400

    cooler than toms code

--- app/public/api/assignment/index.php
+++ app/public/api/assignment/index.php
@@ -8,6 +8,16 @@ $db = DbConnection::getConnection();
 $sql = 'SELECT * FROM assignments';
 $vars = [];
 
+if (isset($_GET['referees'])) {
+    // This is an example of a parameterized query
+    $sql = 'SELECT * FROM assignments WHERE refereeId = ?';
+  
+    //NOT THIS WAY
+    // $sql = 'SELECT * FROM offer WHERE studentId = ' . $_GET['student'];
+  
+    $vars = [ $_GET['referees'] ];
+  }
+
 $stmt = $db->prepare($sql);
 $stmt->execute($vars);
 
--- app/public/js/refmatchhistory.js
+++ app/public/js/refmatchhistory.js
@@ -1,34 +1,28 @@
 const SomeApp = {
     data() {
       return {
-        matches: [],
-        matchForm:{},
-        selectedMatch: null,
         referees: [],
-        refereeForm:{},
-        selectedReferee: null,  
+        selectedReferee: null,
+        assignments: [],
+        selectedAssignment: null
       }
     },
     computed: {},
     methods: {
-        fetchMatchesData() {
-            fetch('/api/match/')
-            .then( response => response.json() )
-            .then( (responseJson) => {
-                console.log(responseJson);
-                this.matches = responseJson;
+        fetchAssignmentsData(s) {
+          console.log("Fetching offer data for ", s);
+          fetch('/api/assignment/?referees=' + s.refereeId)
+          .then( response => response.json() )
+          .then( (responseJson) => {
+              console.log(responseJson);
+              this.assignments = responseJson;
             })
             .catch( (err) => {
                 console.error(err);
             })
-        },
-        selectMatches(s) {
-          if (s == this.selectedMatch) {
-              return;
-          }
-          this.selectedMatch = s;
-          this.matches = [];
-          this.fetchMatchesData(this.selectedMatch);
+            .catch( (error) => {
+                console.error(error);
+            });
       },
       fetchRefereesData() {
         fetch('/api/referee/')
@@ -46,15 +40,8 @@ const SomeApp = {
           return;
       }
       this.selectedReferee = s;
-      this.referees = [];
-      this.fetchRefereesData(this.selectedReferee);
-  },
-      postMatches(evt) {
-        if (this.selectedMatch === null) {
-            this.postNewMatch(evt);
-        } else {
-            this.postEditMatch(evt);
-        }
+      this.assignments = [];
+      this.fetchAssignmentsData(this.selectedReferee);
       },
         postNewMatch(evt) {        
           console.log("Posting!", this.matchForm);
@@ -76,33 +63,8 @@ const SomeApp = {
               this.matchForm = {};
             });
         },
-        postEditMatch(evt) {        
-        this.matchForm.matchId = this.selectedMatch.matchId;        
-          console.log("Updating!", this.matchForm);
-  
-          fetch('api/match/update.php', {
-              method:'POST',
-              body: JSON.stringify(this.matchForm),
-              headers: {
-                "Content-Type": "application/json; charset=utf-8"
-              }
-            })
-            .then( response => response.json() )
-            .then( json => {
-              console.log("Returned from post:", json);
-              // TODO: test a result was returned!
-              this.matches = json;
-              
-              this.resetMatchForm();
-            });
-        },
-        selectMatchForEdit(o) {
-          this.selectedMatch = o;
-          this.matchForm = Object.assign({}, this.selectedMatch);
-      },
   },
     created() {
-        this.fetchMatchesData();
         this.fetchRefereesData();
     }
   
