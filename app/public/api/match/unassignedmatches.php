054a2e10d0fea261b05aee39c0726592c846e7fb
MykahN <89401136+MykahN@users.noreply.github.com>
2021-11-07 20:40:28 -0500

    Added future date filtering

--- app/public/api/match/unassignedmatches.php
+++ app/public/api/match/unassignedmatches.php
@@ -5,7 +5,7 @@ require 'class/DbConnection.php';
 $db = DbConnection::getConnection();
 
 // Step 2: Create & run the query
-$sql = 'SELECT * FROM matches WHERE matchId in (SELECT matchId FROM assignments WHERE status="Unassigned")';
+$sql = 'SELECT * FROM matches WHERE matchId in (SELECT matchId FROM assignments WHERE status="Unassigned") AND matchDate > SYSDATE()';
 $vars = [];
 
 $stmt = $db->prepare($sql);
